a9d4f4d8c50abd53b30b4f29b0e41cb83c8424f1
jarodlopez <127226881+jarodlopez@users.noreply.github.com>
2025-12-23 01:26:28 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'server', // Habilitamos SSR para datos en tiempo real
-  adapter: vercel(),
+  output: 'static',
   integrations: [
     react(),
     tailwind({
